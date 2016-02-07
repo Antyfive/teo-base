c2b46843c5c2b5b3f544e126f63c6fa14a0fe9d2
Andrew Teologov <teologov.and@gmail.com>
2016-02-07 16:16:26 +0200

    adding Teo.JS base class implementation

---
+++ index.js
@@ -0,0 +1,11 @@
+/*!
+ * Teo.JS base class implementation
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 2/7/16
+ */
+
+"use strict";
+
+const Base = require("./lib/teo.base");
+
+module.exports = Base;
